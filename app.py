d41707824a82727a8f48472feab7df61bdddaa12
Felipe Ribeiro <felipinhooliveira06@gmail.com>
2021-01-05 14:48:04 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import random
+
+print('----------------------')
+print('SIMULADOR DE DADO')
+print('')
+print('DIGITE 1 PARA JOGAR')
+print('----------------------')
+
+Answer = input()
+Number = random.randrange(0,7)
+
+print('O numero que foi sorteado é:', Number)
